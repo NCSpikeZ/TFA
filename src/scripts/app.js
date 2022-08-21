2dd4ca7e91f1d45cc97b0a670a0864888265c3ec
Nicolas Coopman <nicolas.coopman@student.heaj.be>
2022-08-21 13:37:33 +0200

    Landing Page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,44 @@
 "use strict";
+/*Copyright*/
+let year = new Date().getFullYear();
+let date = document.querySelector("#date");
+date.innerHTML = "©SpikeZ"+ String(year);
+
+/*Scroll*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+/*Dark Mode*/
+const btnDark = document.querySelector(".btn__darkmode");
+btnDark.addEventListener("click", function(){
+    if(document.body.dataset.theme === "dark"){
+        light();
+        localStorage.setItem("theme", "light");
+    } else {
+        dark();
+        localStorage.setItem("theme", "dark");
+    } 
+});
+
+let theme = localStorage.getItem('theme');
+if((!theme && userDark) || (theme === "dark")){
+    dark();
+    
+} else if(theme === "light"){
+    light();
+}
+
+function dark(){
+    document.body.setAttribute("data-theme", "dark");
+}
+
+function light(){
+    document.body.setAttribute("data-theme", "light");
+}
