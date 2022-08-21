b9eb9c85b33ad986f45e208c7ddf9ad75584e73f
Nicolas Coopman <nicolas.coopman@student.heaj.be>
2022-08-21 18:50:24 +0200

    Page d'accueil 'fin'

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,44 +8,6 @@ let year = new Date().getFullYear();
 let date = document.querySelector("#date");
 date.innerHTML = "©SpikeZ"+ String(year);
 
-/*Scroll*/
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-/*Dark Mode*/
-const btnDark = document.querySelector(".btn__darkmode");
-btnDark.addEventListener("click", function(){
-    if(document.body.dataset.theme === "dark"){
-        light();
-        localStorage.setItem("theme", "light");
-    } else {
-        dark();
-        localStorage.setItem("theme", "dark");
-    } 
-});
-
-let theme = localStorage.getItem('theme');
-if((!theme && userDark) || (theme === "dark")){
-    dark();
-    
-} else if(theme === "light"){
-    light();
-}
-
-function dark(){
-    document.body.setAttribute("data-theme", "dark");
-}
-
-function light(){
-    document.body.setAttribute("data-theme", "light");
-}
 
 gsap.from(".tel1", {
     y: -200,
@@ -59,8 +21,7 @@ gsap.to(".Wrapcontainer", {
         trigger: '.Wrapcontainer',
         pin: true,
         start: 'center center',
-        end: '+=4000',
-        scrub: true,
-        toggleActions: 'play none reverse none'
+        end: 'bottom',
+        scrub: 1,
     }
 });
