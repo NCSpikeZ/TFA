7decf65c05d71133dc2d89bf0f862823688e1376
Nicolas Coopman <nicolas.coopman@student.heaj.be>
2022-08-22 03:14:04 +0200

    fin

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,9 @@
+"use strict";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-"use strict";
+
 /*Copyright*/
 let year = new Date().getFullYear();
 let date = document.querySelector("#date");
@@ -25,3 +26,21 @@ gsap.to(".Wrapcontainer", {
         scrub: 5,
     }
 });
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
